966b4bbef5f4eccd3830ac6e237be69b198823d4
Lian Thompson <lianythompson@gmail.com>
2019-06-12 15:42:17 -0700

    add id library

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 import './App.css';
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Separate recycling',
         completed: true
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Feed cat',
         completed: false
       },
@@ -42,7 +43,12 @@ delTodo = (id) => {
 
 // Add Todo
 addTodo = (title) => {
-  console.log(title)
+  const newTodo = {
+    id: uuid.v4(),
+    title: title,
+    completed: false
+  }
+  this.setState({ todos: [...this.state.todos, newTodo] })
 }
 
 render() {
