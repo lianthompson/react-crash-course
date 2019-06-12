b4b952fe13a535ea051b9383c3df828724efb7fe
Lian Thompson <lianythompson@gmail.com>
2019-06-12 12:59:52 -0700

    add minimal styling

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       {
         id: 2,
         title: 'Separate recycling',
-        completed: false
+        completed: true
       },
       {
         id: 3,
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+    getStyle = () => {
+        // Ternary version of commented out code below
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #CCC dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+        // if(this.props.todo.completed) {
+        //     return {
+        //         textDecoration: 'line-through'
+        //     }
+        // } else {
+        //     return {
+        //         textDecoration: 'none'
+        //     }
+        // }
+    }
+
     render() {
         return (
-            <div>
+            <div style={this.getStyle()}>
                 <p>{ this.props.todo.title }</p>
             </div>
         )
@@ -14,6 +33,10 @@ export class TodoItem extends Component {
 // PropTypes
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
+}
+
+const itemStyle = {
+    backgroundColor: '#f4f4f4'
 }
 
 export default TodoItem
