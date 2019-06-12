45e0434453b569407342d9f6629e950b6378022f
Lian Thompson <lianythompson@gmail.com>
2019-06-12 16:04:36 -0700

    add links to navbar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <header style={headerStyle}>
             <h1>TodoList</h1>
+            <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
         </header>
     )
 }
@@ -14,3 +16,8 @@ const headerStyle = {
     textAlign: 'center',
     padding: '10px'
 } 
+
+const linkStyle = {
+    color: "#fff",
+    textDecoration: 'none'
+}
