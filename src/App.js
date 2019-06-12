411765295aa6186a8f539f757cc6f7b0a054e771
Lian Thompson <lianythompson@gmail.com>
2019-06-12 16:18:10 -0700

    axios delete

--- src/App.js
+++ src/App.js
@@ -30,7 +30,8 @@ markComplete =(id) => {
 
 // Delete Todo
 delTodo = (id) => {
-  this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]
+   }));
 }
 
 // Add Todo
